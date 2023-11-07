a9b1786d3f1a496b008b7009d274c1a53ec60d12
Jayjeet1013 <jayjeet1013@gmail.com>
2023-11-07 14:12:31 +0530

    update menu

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,53 +1,92 @@
-import { Box, Container, Flex, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Text,
+} from "@chakra-ui/react";
 import Image from "next/image";
 import React from "react";
-import {BiChevronDown} from 'react-icons/bi'
-
+import { useState } from "react";
+import { BiChevronDown } from "react-icons/bi";
 
 const Nav = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpansion = () => {
+    setExpanded(!expanded);
+  };
+
+  const menuWidth = expanded ? "800px" : "auto";
+
   return (
-    <Box bg={'skyblue'} pb={10}>
-      <Container maxW={1300} textColor={'white'}>
-        <Box pt={4} >
-          <Flex gap={8} justifyContent={'space-between'} >
-            <Box >
-           <Image src='./esalink.svg' width={200} height={100} alt="image"/>
+    <Box bg={"skyblue"} pb={10}>
+      <Container maxW={1300} textColor={"white"}>
+        <Box pt={4}>
+          <Flex gap={8} justifyContent={"space-between"}>
+            <Box>
+              <Image src="./esalink.svg" width={200} height={100} alt="image" />
             </Box>
 
-            <Flex gap={3}   display={{ base: "none", md: "flex" }}>
+            <Flex gap={3} display={{ base: "none", md: "flex" }}>
               <Text>FACTURATION ÉLECTRONIQUE 2024</Text>
               <Text>Expertise</Text>
               <Text>Solutions</Text>
               <Text>Services</Text>
               <Menu>
-  <MenuButton
-  
-    transition='all 0.2s'
- 
- 
-    _hover={{ bg: 'gray.400' }}
-    _expanded={{ bg: 'blue.400' }}
- 
-  >
-  <Flex align="center">
-        BLog
-    
-          <BiChevronDown />
-   
-      </Flex>
-  </MenuButton>
-  <MenuList>
-    <MenuItem>New File</MenuItem>
-    <MenuItem>New Window</MenuItem>
-    <MenuDivider />
-    <MenuItem>Open...</MenuItem>
-    <MenuItem>Save File</MenuItem>
-  </MenuList>
-</Menu>
-          
+                <MenuButton
+                  onClick={toggleExpansion}
+                  transition="all 0.2s"
+                  _hover={{ bg: "white.400" }}
+                >
+                  <Flex align="center">
+                    Blog
+                    <BiChevronDown />
+                  </Flex>
+                </MenuButton>
+                <MenuList
+                  textColor="black"
+                  width={menuWidth}
+                  maxW="100%"
+                  overflowX="auto"
+                >
+                  <MenuItem py={10}>
+                    New File 1 : Lorem, ipsum dolor sit amet consectetur
+                    adipisicing elit. Minus dolorem blanditiis ullam excepturi
+                    eligendi quis alias temporibus ut eveniet? Exercitationem,
+                    repudiandae inventore commodi autem placeat quam natus
+                    beatae officiis voluptas.
+                  </MenuItem>
+                  <MenuItem py={10}>
+                    New File 2 : Lorem ipsum, dolor sit amet consectetur
+                    adipisicing elit. Soluta dolores non saepe et, nisi ipsam.
+                    Similique dicta tempora maiores quas, quibusdam,
+                    perspiciatis, ad ullam quidem veritatis ex voluptas possimus
+                    distinctio.
+                  </MenuItem>
+                  <MenuItem py={10}>
+                    New File 2 : Lorem ipsum, dolor sit amet consectetur
+                    adipisicing elit. Soluta dolores non saepe et, nisi ipsam.
+                    Similique dicta tempora maiores quas, quibusdam,
+                    perspiciatis, ad ullam quidem veritatis ex voluptas possimus
+                    distinctio.
+                  </MenuItem>
+                  <MenuItem py={10}>
+                    New File 2 : Lorem ipsum, dolor sit amet consectetur
+                    adipisicing elit. Soluta dolores non saepe et, nisi ipsam.
+                    Similique dicta tempora maiores quas, quibusdam,
+                    perspiciatis, ad ullam quidem veritatis ex voluptas possimus
+                    distinctio.
+                  </MenuItem>
+                </MenuList>
+              </Menu>
             </Flex>
 
-            <Flex gap={3}   display={{ base: "none", md: "flex" }}>
+            <Flex gap={3} display={{ base: "none", md: "flex" }}>
               <Text>CONNEXION TMA</Text>
               <Text>OBTENIR UNE DÉMO</Text>
               <Text>FR</Text>
@@ -55,28 +94,41 @@ const Nav = () => {
           </Flex>
         </Box>
 
-        <Flex justifyContent={'space-between'} direction={{base:'column',md:'row'}}>
-      <Flex direction={'column'} gap={4}>
-        <Box w={40} py={6} >  <Image src="/iso.jpg" alt="globe image" width={600} height={1} /> </Box>
-    <Text fontSize={{base:'32px',md:'65px'}} width={'90%'}>La dématérialisation    au service de votre business
-</Text>
+        <Flex
+          justifyContent={"space-between"}
+          direction={{ base: "column", md: "row" }}
+        >
+          <Flex direction={"column"} gap={4}>
+            <Box w={40} py={6}>
+              {" "}
+              <Image
+                src="/iso.jpg"
+                alt="globe image"
+                width={600}
+                height={1}
+              />{" "}
+            </Box>
+            <Text fontSize={{ base: "32px", md: "65px" }} width={"90%"}>
+              La dématérialisation au service de votre business
+            </Text>
 
-<Text fontSize={20}>
-Esalink vous accompagne dans la digitalisation de vos flux B2B
-</Text>
+            <Text fontSize={20}>
+              Esalink vous accompagne dans la digitalisation de vos flux B2B
+            </Text>
 
-<Flex gap={4}> 
-        <Text>Trouver votre solution EDI</Text>
-        <Text>
-        Passer à la facturation électronique
-
-        </Text>
-</Flex>
-
-      </Flex>
-        <Box display={{base:'none',md:'flex'}}>
-  <Image src={'./globe.svg'} alt="globe image" width={600} height={200} />
-        </Box>
+            <Flex gap={4}>
+              <Text>Trouver votre solution EDI</Text>
+              <Text>Passer à la facturation électronique</Text>
+            </Flex>
+          </Flex>
+          <Box display={{ base: "none", md: "flex" }}>
+            <Image
+              src={"./globe.svg"}
+              alt="globe image"
+              width={600}
+              height={200}
+            />
+          </Box>
         </Flex>
       </Container>
     </Box>
