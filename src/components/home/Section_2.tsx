01dc50b19ea960abfce90f54a25865ed1a0538e9
Jayjeet1013 <jayjeet1013@gmail.com>
2023-11-07 14:32:02 +0530

    make more responsive

--- src/components/home/Section_2.tsx
+++ src/components/home/Section_2.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Divider, Flex, Text } from "@chakra-ui/react";
+import { Box, Center, Container, Divider, Flex, Text } from "@chakra-ui/react";
 import Image from "next/image";
 import React from "react";
 
@@ -6,48 +6,57 @@ const Section_2 = () => {
   return (
     <Box pt={24}>
       <Container maxW={1300}>
-        <Flex direction={"column"}>
+        <Flex direction={"column"} gap={4}>
           <Text>TRELLO 101</Text>
-          <Text>A productivity powerhouse</Text>
-          <Text>
+          <Text fontSize={{ base: "24px", md: "36px" }} fontWeight={600}>
+            A productivity powerhouse
+          </Text>
+          <Text w={{ base: "100%", md: "60%" }} fontSize={"20px"}>
             Simple, flexible, and powerful. All it takes are boards, lists, and
             cards to get a clear view of who doing what and what needs to get
             done. Learn more in our guide for getting started.
           </Text>
         </Flex>
-
-        <Flex p={10} direction={{base:'column',md:'row'}}>
-          <Flex direction={"column"}>
-            <Box width={{base:'90%',md:'60%'}}>
-              <Text>Boards</Text>
-              <Text>
-                Trello boards keep tasks organized and work moving forward. In a
-                glance, see everything from “things to do” to “aww yeah, we did
-                it?”
-              </Text>
-            </Box>
-            <Box width={{base:'90%',md:'60%'}}>
-              <Text>Lists</Text>
-              <Text>
-                Trello boards keep tasks organized and work moving forward. In a
-                glance, see everything from “things to do” to “aww yeah, we did
-                it?”
-              </Text>
-            </Box>
-            <Box width={{base:'90%',md:'60%'}}>
-              <Text>Cards</Text>
-              <Text>
-                Trello boards keep tasks organized and work moving forward. In a
-                glance, see everything from “things to do” to “aww yeah, we did
-                it?”
-              </Text>
+        <Center>
+          <Flex p={10} gap={6} direction={{ base: "column", md: "row" }}>
+            <Flex direction={"column"} gap={4} pt={10}>
+              <Box>
+                <Text py={3}>Boards</Text>
+                <Text>
+                  Trello boards keep tasks organized and work moving forward. In
+                  a glance, see everything from “things to do” to “aww yeah, we
+                  did it?”
+                </Text>
+              </Box>
+              <Box>
+                <Text py={3}>Lists</Text>
+                <Text>
+                  The different stages of a task. Start as simple as To Do,
+                  Doing or Done—or build a workflow custom fit to your team’s
+                  needs. There’s no wrong way to Trello.
+                </Text>
+              </Box>
+              <Box>
+                <Text py={3}>Cards</Text>
+                <Text>
+                  Cards represent tasks and ideas and hold all the information
+                  to get the job done. As you make progress, move cards across
+                  lists to show their status.
+                </Text>
+              </Box>
+            </Flex>
+            <Box pt={8}>
+              <Image
+                src={"/imgsec.png"}
+                alt="img sec2"
+                width={1800}
+                height={10}
+              />
             </Box>
           </Flex>
-          <Box pt={8}>
-            <Image src={"/imgsec.png"} alt="img sec2" width={500} height={10} />
-          </Box>
-        </Flex>
-        <Divider/>
+        </Center>
+
+        <Divider />
       </Container>
     </Box>
   );
--- src/components/home/Section_3.tsx
+++ src/components/home/Section_3.tsx
@@ -5,27 +5,27 @@ import React from 'react'
 const Section_3 = () => {
   return (
 <Box py={10}>
-<Container maxW={1300}>
-        
-<Flex justifyContent={'space-between'} direction={{base:'column',md:'row'}} gap={10}>
+<Container maxW={1300} py={20}>
+       
+<Flex px={2} justifyContent={'space-between'} direction={{base:'column',md:'row'}} gap={10}>
         <Center>
 <Box>
         <Image src={'/mobile.png'} width={200} height={10} alt='mobile image'/>
 </Box>
 </Center>
-<Flex direction={'column'}>
-<Text>Never Waste Another Lead</Text>
+<Flex direction={'column'} gap={8}>
+<Text fontSize={{base:'24px',md:'36px'}} fontWeight={600}>Never Waste Another Lead</Text>
 <Box>
-        <Text>Enforced lead updates</Text>
-        <Text>After every call, sales agents have to update the lead status before moving on to the next lead.</Text>
+        <Text fontSize={{base:'18px',md:'24px'}}>Enforced lead updates</Text>
+        <Text fontSize={{base:'14px',md:'21px'}} >After every call, sales agents have to update the lead status before moving on to the next lead.</Text>
 </Box>
 <Box>
-        <Text>Mobile notifications for all your follow ups</Text>
-        <Text>Easily add follow up reminders for your leads to ensure you do lose any deals.</Text>
+        <Text fontSize={{base:'18px',md:'24px'}}>Mobile notifications for all your follow ups</Text>
+        <Text fontSize={{base:'14px',md:'21px'}}>Easily add follow up reminders for your leads to ensure you do lose any deals.</Text>
 </Box>
 <Box>
-        <Text>Conversation history for every lead</Text>
-        <Text>View history of calls, WhatsApp messages, and notes for every lead.</Text>
+        <Text fontSize={{base:'18px',md:'24px'}}>Conversation history for every lead</Text>
+        <Text fontSize={{base:'14px',md:'21px'}}>View history of calls, WhatsApp messages, and notes for every lead.</Text>
 </Box>
 </Flex>
 </Flex>
